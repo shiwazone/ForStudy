1c38e34c2fc44d1c220365b834ae8b53cea9fee6
SummerLiu <15502584710@163.com>
2020-02-24 23:28:15 +0800

    fix clear.bat delete shell script bug

--- DesignPattern/build/clear.bat
+++ DesignPattern/build/clear.bat
@@ -1,4 +1,4 @@
-for /f "delims=" %%i in ('dir/b/s/a-d^|findstr/ev "\.bat | CMakeLists.txt"') do del %%i
+for /f "delims=" %%i in ('dir/b/s/a-d^|findstr/ev "\.bat | \.sh | CMakeLists.txt"') do del %%i
 rmdir /s/q .\bin
 rmdir /s/q .\lib
 rmdir /s/q .\CMakeFiles
